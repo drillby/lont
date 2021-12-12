3b5f0645192707c6be21edafe18e8179af36a975
Pavel Podrazký <podrazkypavel002@gmail.com>
2021-12-12 21:22:36 +0100

    index update

--- scripts.js
+++ scripts.js
@@ -29,3 +29,8 @@ function checkDate() {
 function fiktivniFirma(){
     alert("Jsme fiktivní firma.")
 }
+function podminky(){
+    alert(
+        'V ceně je zahrnuto: \n prostorové vybavení sálu: \n stolky + židle \n malé pódium \n šatna \n technické vybavení sálu: \n projektor + plátno \n ozvučení a osvětlení sálu \n mikrofony. \n Ceny jsou uvedeny pro skupiny do 30 osob, v případě početnější skupiny bude cena navýšena. \n Pokud budete chtít vícedenní akci, cena se zvýší. Pokud u nás budte zůstávat déle kvůli vícedenní akci, dostanete slevu na ubytování. \n Občarstvení není zahrnuto v ceně, pokud o něj budete mít zájem, dejte nám vědět předem.'
+    )
+}
